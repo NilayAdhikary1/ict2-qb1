778e9212dd323f7b4c65f047864c8d08e3af2685
NilayAdhikary1 <126182549+NilayAdhikary1@users.noreply.github.com>
2023-04-17 17:05:56 +0530

    Update App.js
    updated app component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-
 import './App.css';
-import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import { BrowserRouter,Navigate,Outlet,Route, Routes } from 'react-router-dom';
 import Body from './Components/SignInPage/Body/Body';
 import Home from '../src/Components/Home/Home'
+import { useState } from 'react';
+
+const PrivateRoute = (userAuthenticated, ...props) => {
+  return userAuthenticated?
+    <>
+      <Outlet/>
+    </>
+  :
+    <Navigate replace to='/login'/>
+}
 
 function App() {
+
+  const [userAuthenticated, isUserAuthenticated] = useState(false);
 
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path='/login' element={<Body />}/>
-          <Route path='/' element={<Home />}/>    
+          <Route path='/login' element={<Body isUserAuthenticated={isUserAuthenticated}/>} />
+          
+          <Route path='/' element={<PrivateRoute userAuthenticated={userAuthenticated}/>}>
+              <Route path='/' element={<Home/>}/>
+          </Route> 
         </Routes>
       </div>
     </BrowserRouter>

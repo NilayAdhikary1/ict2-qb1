b8c29ecac61eaf479cbcc808169d4288dc4ec492
NilayAdhikary1 <126182549+NilayAdhikary1@users.noreply.github.com>
2023-04-17 16:53:14 +0530

    Update App.js
    updated app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
 import Body from './Components/SignInPage/Body/Body';
+import Home from '../src/Components/Home/Home'
 
 function App() {
+
   return (
     <BrowserRouter>
       <div className="App">
-        <Body/>
+        <Routes>
+          <Route path='/login' element={<Body />}/>
+          <Route path='/' element={<Home />}/>    
+        </Routes>
       </div>
     </BrowserRouter>
   );
